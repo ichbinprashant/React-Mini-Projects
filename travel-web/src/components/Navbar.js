ecfabf3b1071ff306102f8e70c928dbb41a25ea9
ichbinprashant <mishraprashant304@gmail.com>
2022-06-15 21:15:11 +0530

    new

---
+++ travel-web/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <img src="./images/globe.png" className="nav--icon" />
+            <span className="nav--title">my travel journal</span>
+        </nav>
+    )
+}
